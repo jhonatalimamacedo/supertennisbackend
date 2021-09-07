a7324a4b7b5b27f3126febfc85bf1c545b565228
jhonata <jhonata.limamacedoti@gmail.com>
2021-09-07 18:01:49 -0300

    inicio

---
+++ supertennisbackend/supertennisbackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace supertennisbackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\UseSqlServer;Database=Test");
+        }
+    }
+}
